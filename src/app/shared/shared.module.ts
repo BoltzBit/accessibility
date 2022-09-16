5b6eab973f3cc0ca31093c3d1778c6626debb0dc
mateus.silva <mateus.silva@iatec.com>
2022-09-16 08:49:24 -0300

    init accessebility course

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormModule } from '../form/form.module';
+
+
+
+@NgModule({
+    declarations: [],
+    imports: [
+        CommonModule,
+        FormModule
+    ],
+    exports: [
+        FormModule
+    ]
+})
+export class SharedModule { }
