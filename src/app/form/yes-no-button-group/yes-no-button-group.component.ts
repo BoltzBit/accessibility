24b2bc834a7ca10c35182a4bb27ebf5864a1b1e6
mateus.silva <mateus.silva@iatec.com>
2022-09-19 08:38:13 -0300

    Improvement yes-not-component

--- src/app/form/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/form/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,17 +1,31 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+
+enum YesNoButtonGroupOptions{
+    YES = 'yes',
+    NO = 'no'
+}
 
 @Component({
     selector: 'app-form-yes-no-button-group',
     templateUrl: './yes-no-button-group.component.html',
     styleUrls: ['./yes-no-button-group.component.scss']
 })
+
 export class YesNoButtonGroupComponent implements OnInit {
     @Input() value!: string;
     @Input() label!: string;
+    @Output() valueChange =  new EventEmitter<string>();
+
+    public options = YesNoButtonGroupOptions;
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
+    public activate(value: string): void{
+        this.value = value;
+
+        this.valueChange.emit(this.value);
+    }
 }
