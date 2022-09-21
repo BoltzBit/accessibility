962b1748bc3ce026d098857a2281c18e1d2d522d
mateus.silva <mateus.silva@iatec.com>
2022-09-21 00:42:49 -0300

    Improvement reactive form model

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
     selector: 'app-root',
@@ -6,6 +7,16 @@ import { Component } from '@angular/core';
     styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-    public yesNoAnswer = "no";
+    public form!: FormGroup;
     title = 'accessibility';
+
+    constructor(private _formBuilder: FormBuilder){
+        this.form = this._formBuilder.group({
+            yesNoAnswer: ['no']
+        });
+    }
+
+    public submit(): void{
+        console.log(this.form.value)
+    }
 }
--- src/app/form/yes-no-button-group/yes-no-button-group.component.ts
+++ src/app/form/yes-no-button-group/yes-no-button-group.component.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, forwardRef, Input, OnInit, Output } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
 
 enum YesNoButtonGroupOptions{
     YES = 'yes',
@@ -8,24 +9,52 @@ enum YesNoButtonGroupOptions{
 @Component({
     selector: 'app-form-yes-no-button-group',
     templateUrl: './yes-no-button-group.component.html',
-    styleUrls: ['./yes-no-button-group.component.scss']
+    styleUrls: ['./yes-no-button-group.component.scss'],
+    providers: [
+        {
+            provide: NG_VALUE_ACCESSOR,
+            multi: true,
+            useExisting: forwardRef(() => YesNoButtonGroupComponent)
+        }
+    ]
 })
 
-export class YesNoButtonGroupComponent implements OnInit {
+//dizer pro componente implementar a interface que interage com o FormGroup
+export class YesNoButtonGroupComponent implements OnInit, ControlValueAccessor {
     @Input() value!: string;
     @Input() label!: string;
     @Output() valueChange =  new EventEmitter<string>();
 
     public options = YesNoButtonGroupOptions;
+    public onChange = (value: string) => {};
+    public onTouched = () => {};
 
     constructor() { }
 
     ngOnInit(): void {
     }
 
-    public activate(value: string): void{
+    //cara que recebe o valor e passa pro componente
+    public writeValue(value: string): void {
         this.value = value;
-
+        this.onChange(this.value);
         this.valueChange.emit(this.value);
+    }
+
+    //cara que monitora as mudanças do componente
+    public registerOnChange(fn: (value: string) => void): void {
+        this.onChange = fn;
+    }
+
+    public registerOnTouched(fn: () => void): void {
+        this.onTouched = fn;
+    }
+
+    public setDisabledState(isDisabled: boolean): void {
+        
+    }
+
+    public activate(value: string): void{
+        this.writeValue(value);
     }
 }
