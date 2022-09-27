e0679c9ba9a14869550b9c37604c48775f66b4f4
mateus.silva <mateus.silva@iatec.com>
2022-09-27 08:51:28 -0300

    Add directive keyboard-manager

--- src/app/form/form.module.ts
+++ src/app/form/form.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { YesNoButtonGroupComponent } from './yes-no-button-group/yes-no-button-group.component';
+import { KeyboardManagerModule } from '../shared/directives/key-manager/keyboard-manager.module';
 
 
 
@@ -9,7 +10,8 @@ import { YesNoButtonGroupComponent } from './yes-no-button-group/yes-no-button-g
         YesNoButtonGroupComponent
     ],
     imports: [
-        CommonModule
+        CommonModule,
+        KeyboardManagerModule
     ],
     exports: [
         YesNoButtonGroupComponent
